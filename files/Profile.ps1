2a7c6d1ed6ec744a8c9c9a56a4686f12cef0593d
Jakub Sokołowski <jakub@status.im>
2020-10-01 23:06:36 +0200

    expand PowerShell profile
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- files/Profile.ps1
+++ files/Profile.ps1
@@ -1,2 +1,24 @@
+# Use vi movements
 Set-PSReadlineOption -EditMode Vi
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# History and file search shortcuts
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t'
+Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Bash-like shorthands
+function ll { dir }
+function htop { ntop }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5    { Get-FileHash -Algorithm MD5 $args }
+function sha1   { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Shorthand for Git
+function g {
+    if ($args.coung > 0) {
+        git $args
+    } else {
+        git status
+    }
+}
